0cb351e9ac89e338050e4f0b44fc354159880ba4
Stefan Aikins <stefanaikins@gmail.com>
2020-10-29 15:35:54 +0000

    series of prompt displays when button is cliked

--- script.js
+++ script.js
@@ -121,3 +121,30 @@ generateBtn.addEventListener('click', generatePassword);
 
 */
 
+//ask user password criteria to include
+
+//password generate button
+var generateBtn = document.querySelector('#generate');
+
+//the click event
+generateBtn.addEventListener('click', generatePassword);
+
+//password criteria
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharaters = ['#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '['];
+
+//1. present user with series of prompts for password criteria
+
+function generatePassword() {
+    prompt('uppercase')
+    prompt('lowercase')
+    prompt('numbers')
+    prompt('special characters')
+}
+
+
+
+
+
